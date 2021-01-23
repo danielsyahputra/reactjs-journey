0d2b53dea189c6e6d965027dc5393a0dc43bfa07
danielsyahputra13 <dnlshp@gmail.com>
2021-01-23 11:16:32 +0700

    learn about component and JSX in React

--- reactjs-app/src/App.js
+++ reactjs-app/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App!</h1>
+        <p>This is really working!</p>
+        <Person />
+        <Person />
+        <Person />
+      </div>
+    );
+
+    // This is another way to define jsx
+    // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Does this work now?'));
+  }
 }
 
 export default App;
+
+/**
+ * Note: our JSX element can just have one root element
+ */
