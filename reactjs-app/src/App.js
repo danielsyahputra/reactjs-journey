e32725e5c07f216a7b68da71822c695e5910d988
danielsyahputra13 <dnlshp@gmail.com>
2021-01-24 17:58:30 +0700

    understanding props

--- reactjs-app/src/App.js
+++ reactjs-app/src/App.js
@@ -4,13 +4,43 @@ import React, { Component } from 'react';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Daniel', age: 19 },
+      { name: 'Harry', age: 20 },
+      { name: 'Hermione', age: 19},
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    // Don't do this
+    //this.state.persons[0] = 'Daniel Syahputra';
+    this.setState({
+      persons: [
+        { name: "Daniel Syahputra", age: 19 },
+        { name: "Harry", age: 20 },
+        { name: "Hermione", age: 19 },
+      ],
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
-        <Person name="Daniel" age="19" />
-        <Person name="Harry" age="20">My Hobbies: Programming</Person>
+        <button onClick={this.switchNameHandler}>Swith Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies: Programming
+        </Person>
       </div>
     );
 
