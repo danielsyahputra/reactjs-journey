50d3a345e54167fb4d47098e11a49a3b173a696e
danielsyahputra13 <dnlshp@gmail.com>
2021-01-24 18:25:57 +0700

    understanding functional and class component

--- reactjs-app/src/App.js
+++ reactjs-app/src/App.js
@@ -1,52 +1,52 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
-      { name: 'Daniel', age: 19 },
-      { name: 'Harry', age: 20 },
-      { name: 'Hermione', age: 19},
-    ]
-  }
+      { name: "Daniel", age: 19 },
+      { name: "Harry", age: 20 },
+      { name: "Hermione", age: 19 },
+    ],
+  });
 
-  switchNameHandler = () => {
+  const [otherState, setOtherState] = useState({ otherStates: 'some other value' });
+
+  // Di functional component, ketika kita memutasi component maka semua states nya akan di override
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
     // console.log('Was clicked!');
     // Don't do this
     //this.state.persons[0] = 'Daniel Syahputra';
-    this.setState({
+    setPersonsState({
       persons: [
         { name: "Daniel Syahputra", age: 19 },
         { name: "Harry", age: 20 },
         { name: "Hermione", age: 19 },
       ],
     });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App!</h1>
-        <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Swith Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          My Hobbies: Programming
-        </Person>
-      </div>
-    );
-
-    // This is another way to define jsx
-    // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Does this work now?'));
-  }
+  };
+  
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App!</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Swith Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My Hobbies: Programming
+      </Person>
+    </div>
+  );
 }
 
 export default App;
