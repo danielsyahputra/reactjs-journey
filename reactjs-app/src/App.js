adf29c47d6eaabfa42ff3bc433d95cc1fe3d4a06
danielsyahputra13 <dnlshp@gmail.com>
2021-01-24 18:59:03 +0700

    back again to class based component

--- reactjs-app/src/App.js
+++ reactjs-app/src/App.js
@@ -1,57 +1,67 @@
-// import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
-const App = props => {
-  const [ personsState, setPersonsState ] = useState({
+class App extends Component {
+  state = {
     persons: [
-      { name: "Daniel", age: 19 },
-      { name: "Harry", age: 20 },
-      { name: "Hermione", age: 19 },
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 },
     ],
-  });
+    otherState: "some other value",
+  };
 
-  const [otherState, setOtherState] = useState({ otherStates: 'some other value' });
-
-  // Di functional component, ketika kita memutasi component maka semua states nya akan di override
-  console.log(personsState, otherState);
-
-  const switchNameHandler = (newName) => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
-    // Don't do this
-    //this.state.persons[0] = 'Daniel Syahputra';
-    setPersonsState({
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
       persons: [
-        { name: newName, age: 19 },
-        { name: "Harry", age: 20 },
-        { name: "Hermione", age: 19 },
+        { name: newName, age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 27 },
       ],
     });
   };
-  
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App!</h1>
-      <p>This is really working!</p>
-      <button onClick={switchNameHandler.bind(this, 'Daniel Syahputra')}>Swith Name</button>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-        click = {switchNameHandler}
-      />
-      <Person
-        name={personsState.persons[1].name}
-        age={personsState.persons[1].age}
-      >
-        My Hobbies: Programming
-      </Person>
-    </div>
-  );
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: "Stephanie", age: 26 },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={() => this.switchNameHandler("Maximilian!!")}>
+          Switch Name
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Max!")}
+          changed={this.nameChangedHandler}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
 }
 
 export default App;
-
-/**
- * Note: our JSX element can just have one root element
- */
