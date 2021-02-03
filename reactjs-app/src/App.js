a57a1db70b959d9815e8de264bcaa91a509c5b59
danielsyahputra13 <dnlshp@gmail.com>
2021-02-03 19:00:26 +0700

    add something

--- reactjs-app/src/App.js
+++ reactjs-app/src/App.js
@@ -35,11 +35,18 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler("Daniel Syahputra!!")}>
+        <button style={style} onClick={() => this.switchNameHandler("Daniel Syahputra!!")}>
           Switch Name
         </button>
         <Person
