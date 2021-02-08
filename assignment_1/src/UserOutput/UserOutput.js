19b30dec88d15a8d322fbcfee3dda0c386893c82
danielsyahputra13 <dnlshp@gmail.com>
2021-02-08 19:51:08 +0700

    :smile: add components for assignment_1

---
+++ assignment_1/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const userOutput = () => {
+    return (
+        <div>
+            <p>Test</p>
+            <p>Test</p>
+        </div>
+    );
+};
+
+export default userOutput;
