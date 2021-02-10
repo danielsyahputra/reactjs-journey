eb3f113ec44ec5283f37a89f6cb206889286eeec
danielsyahputra13 <dnlshp@gmail.com>
2021-02-10 10:03:17 +0700

    :sparkles: adding component for assignment_2

---
+++ assignment_2/src/CharComponent/CharComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const charComponent = () => {
+    return (
+        <div className="CharComponent">
+            <h2>Char Component</h2>
+        </div>
+    )
+}
+
+export default charComponent;
---
+++ assignment_2/src/ValidationComponent/ValidationComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const validationComponent = () => {
+  return (
+    <div className="ValidationComponent">
+      <h2>Validation Component</h2>
+    </div>
+  );
+};
+
+export default validationComponent;
