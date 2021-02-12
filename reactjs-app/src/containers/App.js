37e00af5120a3ff8dbcc494a850f48b618878d19
danielsyahputra13 <dnlshp@gmail.com>
2021-02-12 19:38:48 +0700

    :fire: splitting an App into components

--- reactjs-app/src/containers/App.js
+++ reactjs-app/src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./App.module.css";
-import Person from "../Persons/Person/Person";
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
   state = {
@@ -44,43 +45,23 @@ class App extends Component {
     // All pseudo-selector have to wrapped in quotation marks
 
     let persons = null;
-    let btnClass = '';
-
     if (this.state.showPersons) {
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={(event) => this.nameChangedHandler(event, person.id)}
-              />
-            );
-          })}
-        </div>
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
       );
-      btnClass = classes.Red;
-    }
-
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold);
     }
 
     // Don't forget to wrap our application with StyleRoot component provided by Radium
     return (
       <div className={classes.App}>
-        <h1>Hi, I'm a React App</h1>
-        <p className={assignedClasses.join(" ")}>This is really working!</p>
-        <button className={btnClass} onClick={this.togglePersonsHandler}>
-          Toggle Person
-        </button>
+        <Cockpit
+          clicked={this.togglePersonsHandler}
+          showPersons={this.state.showPersons}
+          persons={ this.state.persons } />
         {persons}
       </div>
     );
